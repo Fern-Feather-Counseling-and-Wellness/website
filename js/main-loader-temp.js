f0d3c68ee7ec2f21587376474cc4d41a2a41462c
Jeeprasr <nicole.e.browning@gmail.com>
2026-09-24 22:45:04 -0400

    Prepare main loader blob

---
+++ js/main-loader-temp.js
@@ -0,0 +1,2 @@
+// exact loader used for main.js
+(function(){var core=document.createElement('script');core.src='/js/main-core.js?v=20260925';core.onload=function(){var photos=document.createElement('script');photos.src='/js/nicole-photo-overrides.js?v=20260925';document.head.appendChild(photos);};document.head.appendChild(core);})();
