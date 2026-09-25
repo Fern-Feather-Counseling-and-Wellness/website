a9f81131b4be1a2239da3ab402ef14ef3a430bee
Jeeprasr <nicole.e.browning@gmail.com>
2026-09-24 22:54:54 -0400

    Reposition Nicole photos to keep face visible

--- js/nicole-photo-overrides.js
+++ js/nicole-photo-overrides.js
@@ -1,16 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var path = window.location.pathname;
+(function () {
+  function applyNicolePhotos() {
+    var path = window.location.pathname;
 
-  if (/\/about\.html$/.test(path)) {
-    var aboutNicole = document.querySelector('.therapist-card img[src*="nicole-portrait"]');
-    if (aboutNicole) aboutNicole.src = 'images/38-IMG_1797.jpg';
+    if (/\/about\.html$/.test(path)) {
+      var aboutNicole = document.querySelector('.therapist-card img');
+      if (aboutNicole) {
+        aboutNicole.src = 'images/38-IMG_1797.jpg';
+        aboutNicole.style.objectPosition = '50% 18%';
+      }
+    }
+
+    if (/\/nicole\.html$/.test(path)) {
+      var nicoleHero = document.querySelector('.therapist-photo img');
+      if (nicoleHero) {
+        nicoleHero.src = 'images/64-IMG_2001.jpg';
+        nicoleHero.style.objectPosition = '50% 18%';
+      }
+
+      var ogImage = document.querySelector('meta[property="og:image"]');
+      if (ogImage) ogImage.content = 'https://fernandfeathercounseling.com/images/64-IMG_2001.jpg';
+    }
   }
 
-  if (/\/nicole\.html$/.test(path)) {
-    var nicoleHero = document.querySelector('.therapist-photo img');
-    if (nicoleHero) nicoleHero.src = 'images/64-IMG_2001.jpg';
-
-    var ogImage = document.querySelector('meta[property="og:image"]');
-    if (ogImage) ogImage.content = 'https://fernandfeathercounseling.com/images/64-IMG_2001.jpg';
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', applyNicolePhotos);
+  } else {
+    applyNicolePhotos();
   }
-});
+})();
