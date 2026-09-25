740b424949d4d9b4f2154ff021d3a1605d83c68f
Jeeprasr <nicole.e.browning@gmail.com>
2026-09-24 22:44:26 -0400

    Add Nicole photo loader

---
+++ js/nicole-photo-loader.js
@@ -0,0 +1,6 @@
+(function () {
+  var script = document.createElement('script');
+  script.src = '/js/nicole-photo-overrides.js?v=20260925';
+  script.defer = true;
+  document.head.appendChild(script);
+})();
