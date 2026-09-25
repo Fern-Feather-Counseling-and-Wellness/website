23da4c9c6d91f66f7f157076e922ce7da2b72844
Jeeprasr <nicole.e.browning@gmail.com>
2026-09-24 23:01:43 -0400

    Fix Nicole About page portrait crop

--- js/nicole-photo-overrides-fixed.js
+++ js/nicole-photo-overrides-fixed.js
@@ -3,7 +3,10 @@
     var path = window.location.pathname;
     if (/\/about(?:\.html)?\/?$/.test(path)) {
       var aboutNicole = document.querySelector('.therapist-card img[src*="nicole-portrait"]');
-      if (aboutNicole) aboutNicole.src = 'images/38-IMG_1797.jpg';
+      if (aboutNicole) {
+        aboutNicole.src = 'images/38-IMG_1797.jpg';
+        aboutNicole.style.objectPosition = 'center 18%';
+      }
     }
     if (/\/nicole(?:\.html)?\/?$/.test(path)) {
       var nicoleHero = document.querySelector('.therapist-photo img');
